6ca0b4516058a06e01ee2cce0fa96c2c25e118bc
Vien Pham <vienpham202@gmail.com>
2017-11-25 22:20:27 +0700

    deploy

--- server.js
+++ server.js
@@ -9,5 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 app.listen(app.get('port'), () => {
-    console.log('Server is running on port 3000')
+    debugger
+    app.get('port')
+    console.log(`Server is running on port ${app.get('port')}`)
 })
