8160097cd10006dc44957ed2a131a5fb5de634e2
Vien Pham <vienpham202@gmail.com>
2017-11-25 11:28:05 +0700

    add UI

---
+++ app/javascripts/auth.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    var isLogin = localStorage.getItem("isLogged");
+    if (isLogin == null || isLogin == false) {
+        $('#qrcodepage').hide();
+        console.log('hide');
+    } else {
+        loggedIn();
+    }
+    var isLogin = localStorage.getItem("isLogged");
+    if (isLogin == null || isLogin == false) {
+        $('#qrcodepage').hide();
+        console.log('hide');
+    } else {
+        loggedIn();
+    }
+
+});
+
+
+window.login = function () {
+
+    console.log($('#exampleInputEmail1').val());
+    console.log($('#exampleInputPassword1').val());
+
+    if ($('#exampleInputEmail1').val() == 'user@gmail.com' && $('#exampleInputPassword1').val() == '123') {
+        console.log('hi');
+        localStorage.setItem("isLogged", 'true');
+        console.log(localStorage);
+        loggedIn();
+    }
+}
+
+window.loggedIn = function () {
+    $('#loginpage').hide();
+    $('#qrcodepage').show();
+}
