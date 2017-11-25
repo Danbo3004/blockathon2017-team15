fc25b73572ae2db0545d4158c4be573497715468
Trung Tran <trungtran07@gmail.com>
2017-11-25 18:26:40 +0700

    Fix conflict

--- truffle.js
+++ truffle.js
@@ -1,30 +1,30 @@
-var ethereumjsWallet = require('ethereumjs-wallet');
-var ProviderEngine = require("web3-provider-engine");
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-var Web3 = require("web3");
-var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
+// var ethereumjsWallet = require('ethereumjs-wallet');
+// var ProviderEngine = require("web3-provider-engine");
+// var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+// var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+// var Web3 = require("web3");
+// var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 
-// replace with your private key
-var privateKey = 'd3fe9d8a8d21208cb0893b4331245304dd22042d6c55c2bb5054a5445e37eda4';
+// // replace with your private key
+// var privateKey = 'd3fe9d8a8d21208cb0893b4331245304dd22042d6c55c2bb5054a5445e37eda4';
 
-// create wallet from existing private key
-var wallet = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
-var address = "0x" + wallet.getAddress().toString("hex");
+// // create wallet from existing private key
+// var wallet = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
+// var address = "0x" + wallet.getAddress().toString("hex");
 
-// using ropsten testnet
-var providerUrl = "http://www.blockathon.asia:8545/";
-var engine = new ProviderEngine();
+// // using ropsten testnet
+// var providerUrl = "http://www.blockathon.asia:8545/";
+// var engine = new ProviderEngine();
 
-// filters
-engine.addProvider(new FilterSubprovider());
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
-engine.on('error', function(err) {
-  // report connectivity errors
-  console.error(err.stack)
-})
-engine.start();
+// // filters
+// engine.addProvider(new FilterSubprovider());
+// engine.addProvider(new WalletSubprovider(wallet, {}));
+// engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
+// engine.on('error', function(err) {
+//   // report connectivity errors
+//   console.error(err.stack)
+// })
+// engine.start();
 
 // See <http://truffleframework.com/docs/advanced/configuration>
 // to customize your Truffle configuration!
@@ -34,12 +34,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    rinkeby: {
-      from: address,
-      provider: engine,
-      network_id: 4,
-      gas: 4600000
     }
+    // ,
+    // rinkeby: {
+    //   from: address,
+    //   provider: engine,
+    //   network_id: 4,
+    //   gas: 4600000
+    // }
   }
 };
