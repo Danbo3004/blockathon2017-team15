0a79c304281bf7b75aae11c20d27f6ddd61a5758
Vien Pham <vienpham202@gmail.com>
2017-11-25 22:13:45 +0700

    deploy n

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('port', process.env.PORT || 3000)
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'))
 })
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port 3000')
+})
